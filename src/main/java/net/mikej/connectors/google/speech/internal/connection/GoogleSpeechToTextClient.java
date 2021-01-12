b6a16603d2e462f8cd241308b19175406c3fec83
Michael Jones <mjones@kotrs.com>
2021-01-12 10:03:36 -0500

    Added icon

---
+++ src/main/java/net/mikej/connectors/google/speech/internal/connection/GoogleSpeechToTextClient.java
@@ -0,0 +1,7 @@
+package net.mikej.connectors.google.speech.internal.connection;
+
+import com.google.cloud.speech.v1.SpeechClient;
+
+public class GoogleSpeechToTextClient {
+	private SpeechClient speechClient;
+}
